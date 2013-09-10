5516dbc4f4b20e2423bf64ab5bd23ec235daad13
nikmolnar <nik.molnar@consbio.org>
2013-09-10 15:06:48 -0700

    Fixed another path bug

--- ags/admin/paths.py
+++ ags/admin/paths.py
@@ -6,7 +6,7 @@ AGS_ADMIN_PATH_PATTERNS = {
     'edit_service': "%(admin_root)s/services/%(service_path)s.%(service_type)s/edit",
     'get_service_item_info': "%(admin_root)s/services/%(service_path)s.%(service_type)s/iteminfo",
     'edit_service_item_info': "%(admin_root)s/services/%(service_path)s.%(service_type)s/iteminfo/edit",
-    'get_service_status': "%(admin_root)s/services/%(service_path)s.(service_type)s/status",
+    'get_service_status': "%(admin_root)s/services/%(service_path)s.%(service_type)s/status",
     'start_service': "%(admin_root)s/services/%(service_path)s.%(service_type)s/start",
     'stop_service': "%(admin_root)s/services/%(service_path)s.%(service_type)s/stop",
     'delete_service': "%(admin_root)s/services/%(service_path)s.%(service_type)s/delete"
