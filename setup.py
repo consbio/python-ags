5d6a8c58aa8a4a7f3f217be135a1a2f19d5a9a74
nikmolnar <nik.molnar@consbio.org>
2014-06-20 12:28:18 -0700

    Bump to version 0.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='python-ags',
-    version='0.2.1',
+    version='0.2.2',
     packages=['ags', 'ags.admin', 'ags.admin.services'],
     requires=["requests"]
 )
